b9fb4faab254b750bb2f59dadc81dc73b8ae97d5
Khomotjo <khomotjomodipa4@gmail.com>
2022-08-29 20:48:09 +0200

    added office grid to home page

--- office-space-manager/src/app/features/home/home.page.ts
+++ office-space-manager/src/app/features/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OfficeModel } from '../../core/models/office.model';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  office: OfficeModel = {
+    name: 'Specno',
+    address: '10 Willie Van Schoor Dr, Bo Oakdale, Cape Town, 7530',
+    email: 'info@specno.com',
+    phone: '0987654321',
+    capacity: 200,
+    hexcolor: 'FE9B71',
+    officeId: 'jsgf',
+  };
 
   constructor() {}
-
 }
